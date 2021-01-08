bd2d25bb7b4227413a75108aa72af50d681855ca
Seshan <zeshan.nandan@gmail.com>
2021-01-09 00:11:18 +0530

    Updated options

--- lib/fancy_cursor.dart
+++ lib/fancy_cursor.dart
@@ -1,6 +1,5 @@
 library fancy_cursor;
 
-import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -13,8 +12,8 @@ class FancyCursor extends StatefulWidget {
   Widget customCursor, trailCustomCursor;
   bool trail;
   double trailOpacity;
-  GlobalKey cursorKey = GlobalKey();
-  GlobalKey trailCursorKey = GlobalKey();
+  GlobalKey customCursorKey = GlobalKey();
+  GlobalKey trailCustomCursorKey = GlobalKey();
 
   FancyCursor({
     Key key,
@@ -38,11 +37,8 @@ class FancyCursor extends StatefulWidget {
     this.trailDelay ??= this.delay + Duration(milliseconds: 300);
     this.trailCurve ??= this.curve;
 
-    if (customCursor != null) {
-      print('Custom cursor');
+    if (this.customCursor != null) {
       assert(this.color == null && this.size == null);
-
-      print('Setting trail custom cursor from custom cursor');
       this.trailCustomCursor ??= Opacity(
         opacity: this.trailOpacity ?? 0.3,
         child: Transform.scale(
@@ -51,22 +47,12 @@ class FancyCursor extends StatefulWidget {
         ),
       );
     } else {
-      print('No custom cursor');
-      print('Setting default cursor attributes');
       this.color ??= Colors.black;
       this.size ??= 8;
 
       if (this.trailCustomCursor != null) {
-        print('Trail custom cursor');
-        if (this.customCursor == null)
-          assert(this.trailColor == null && this.trailSize == null);
-        this.trailCustomCursor = Container(
-          key: trailCursorKey,
-          child: this.trailCustomCursor,
-        );
+        assert(this.trailColor == null && this.trailSize == null);
       } else {
-        print('No trail custom cursor');
-        print('');
         this.trailColor ??= this.color.withOpacity(0.3);
         this.trailSize ??= this.size * 1.5;
       }
@@ -77,142 +63,59 @@ class FancyCursor extends StatefulWidget {
   _FancyCursorState createState() => _FancyCursorState();
 }
 
-class _FancyCursorState extends State<FancyCursor> with AfterLayoutMixin {
+class _FancyCursorState extends State<FancyCursor> {
   Offset position = Offset.zero;
-
-  Size customCursorSize = Size.zero;
-  Size trailCustomCursorSize = Size.zero;
-
-  @override
-  void afterFirstLayout(BuildContext context) {
-    // setState(() {
-    //   customCursorSize =
-    // });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.none,
-      onHover: (phe) {
-        setState(() => position = phe.position);
-      },
-      child: Stack(
-        children: [
-          widget.child,
-          if (widget.trail)
+    return Material(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.none,
+        onHover: (phe) {
+          setState(() => position = phe.position);
+        },
+        child: Stack(
+          children: [
+            widget.child,
+            if (widget.trail)
+              AnimatedPositioned(
+                duration: widget.trailDelay,
+                curve: widget.trailCurve,
+                top: position.dy - ((widget.trailSize ?? 0) / 2),
+                left: position.dx - ((widget.trailSize ?? 0) / 2),
+                child: IgnorePointer(
+                  child: widget.trailCustomCursor ??
+                      Container(
+                        decoration: BoxDecoration(
+                          color: widget.trailColor,
+                          borderRadius:
+                              BorderRadius.circular(widget.trailSize / 2),
+                        ),
+                        height: widget.trailSize,
+                        width: widget.trailSize,
+                      ),
+                ),
+              ),
             AnimatedPositioned(
-              duration: widget.trailDelay,
-              curve: widget.trailCurve,
-              top: position.dy - ((widget.trailSize ?? 0) / 2),
-              left: position.dx - ((widget.trailSize ?? 0) / 2),
+              duration: widget.delay,
+              curve: widget.curve,
+              top: position.dy - (widget.size ?? 0) / 2,
+              left: position.dx - (widget.size ?? 0) / 2,
               child: IgnorePointer(
-                child: widget.trailCustomCursor ??
+                child: widget.customCursor ??
                     Container(
                       decoration: BoxDecoration(
-                        color: widget.trailColor,
-                        borderRadius:
-                            BorderRadius.circular(widget.trailSize / 2),
+                        color: widget.color,
+                        borderRadius: BorderRadius.circular(widget.size / 2),
                       ),
-                      height: widget.trailSize,
-                      width: widget.trailSize,
+                      height: widget.size,
+                      width: widget.size,
                     ),
               ),
             ),
-          AnimatedPositioned(
-            duration: widget.delay,
-            curve: widget.curve,
-            top: position.dy - (widget.size ?? 0) / 2,
-            left: position.dx - (widget.size ?? 0) / 2,
-            child: IgnorePointer(
-              child: widget.customCursor ??
-                  Container(
-                    decoration: BoxDecoration(
-                        color: widget.color,
-                        borderRadius: BorderRadius.circular(widget.size / 2)),
-                    height: widget.size,
-                    width: widget.size,
-                  ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
-
-// {
-//   // if (cursor != null) {
-//   //   assert(this.color == null &&
-//   //       this.size == null &&
-//   //       this.delay == null &&
-//   //       this.curve == null &&
-//   //       this.customCursor == null);
-//   // } else
-//   // this.cursor = CursorProps(
-//   // this.color = cursor.color ?? Colors.black;
-//   // this.size = cursor.size ?? 8;
-//   // this.delay = cursor.size ?? const Duration(milliseconds: 300);
-//   // this.curve = cursor.curve ?? Curves.elasticOut;
-//   // this.customCursor = cursor.customCursor;
-//   // );
-
-//   // if (overlay) {
-//   //   if (overlayCursor != null) {
-//   //     assert(this.overlayColor == null &&
-//   //         this.overlaySize == null &&
-//   //         this.overlayDelay == null &&
-//   //         this.overlayCurve == null &&
-//   //         this.overlayCustomCursor == null);
-//   //   } else {
-//   //     this.overlayCursor = new CursorProps(
-//   //       color: cursor.color.withOpacity(0.5),
-//   //       size: cursor.size * 2,
-//   //       delay: cursor.delay + Duration(milliseconds: 300),
-//   //       curve: cursor.curve,
-//   //       customCursor: cursor.customCursor ??
-//   //           Opacity(
-//   //             opacity: 0.2,
-//   //             child: Transform.scale(
-//   //               scale: 1.2,
-//   //               child: cursor.customCursor,
-//   //             ),
-//   //           ),
-//   //     );
-//   //   }
-//   // } else {
-//   //   assert(overlaySize == null &&
-//   //       overlayColor == null &&
-//   //       overlayCurve == null &&
-//   //       overlayCustomCursor == null &&
-//   //       overlayDelay == null &&
-//   //       overlayCursor == null);
-//   // }
-// }
-
-// FancyCursor.props(
-//     {@required this.child,
-//     CursorProps cursor,
-//     CursorProps overlayCursor,
-// //     bool overlay = true}) {
-//   if (overlay) {
-//     this.overlayColor ??= this.color.withOpacity(0.2);
-//     this.overlaySize ??= this.size * 2;
-//     this.overlayDelay ??= this.delay + Duration(milliseconds: 300);
-//     this.overlayCurve ??= this.curve;
-//     this.overlayCustomCursor ??= this.customCursor ??
-//         Opacity(
-//           opacity: 0.2,
-//           child: Transform.scale(
-//             scale: 1.2,
-//             child: this.customCursor,
-//           ),
-//         );
-//   } else {
-//     assert(overlaySize == null &&
-//         overlayColor == null &&
-//         overlayCurve == null &&
-//         overlayCustomCursor == null &&
-//         overlayDelay == null);
-//   }
-// }
