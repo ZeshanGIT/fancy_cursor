988daf0ecdd8da96e818053abb7ca149d14af2bc
Seshan <zeshan.nandan@gmail.com>
2021-01-08 17:08:52 +0530

    example

---
+++ example/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:fancy_cursor/fancy_cursor.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FancyCursor(
+        child: Container(),
+      ),
+    );
+  }
+}
